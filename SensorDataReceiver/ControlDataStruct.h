37ebecc8591a5f3f5a0008eede02a32c63999509
TwnET <a0919958057@gmail.com>
2016-06-22 16:40:03 +0800

    修改ControlDataStruct  新增協方差矩陣6x6(初始Pose) 和 x,y,z yaw, pitch, row六個向量，並開始開發初始定位控制via AMCL

--- SensorDataReceiver/ControlDataStruct.h
+++ SensorDataReceiver/ControlDataStruct.h
@@ -4,6 +4,7 @@
 
 // The Controll function for Map Provider
 enum ControlMapMsg {
+	SET_INIT_POSE,
 	GET_MAP_LIST,
 	SEL_MAP,
 	LOAD_MAP
@@ -19,5 +20,16 @@ enum ControlLRFMsg {
 struct ControlMsg {
 	ControlMapMsg map_msg;
 	ControlLRFMsg lrf_msg;
+	// position X, Y, Z
+	// (x, y, z)
+	double init_pose_position[3];
+
+	// orientation yaw, pitch, row
+	// (rotation about X axis, rotation about Y axis, rotation about Z axis)
+	double init_pose_orientation[3];
+
+	//(x, y, z, rotation about X axis, rotation about Y axis, rotation about Z axis)
+	double init_pose_cov[36];
+
 	int data[REPORT_DATA_SIZE];
 };
