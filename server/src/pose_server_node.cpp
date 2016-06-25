43f26f1d287768ecafc450d7faa0e7b60f4b0bba
TaiwaET <a0919958057@gmail.com>
2016-06-25 18:45:30 +0800

    Pose server 座標轉移修正

--- server/src/pose_server_node.cpp
+++ server/src/pose_server_node.cpp
@@ -174,8 +174,8 @@ int main(int argc, char** argv) {
       tf::StampedTransform transform;
       try{
 
-        listener.lookupTransform("/base_link",
-                                 "/map", ros::Time(0), transform);
+        listener.lookupTransform("/map",
+                                 "/base_link", ros::Time(0), transform);
       }
       catch (tf::TransformException ex){
         ROS_ERROR("%s",ex.what());
